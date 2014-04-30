65c6d9010e1b09e841d4eb6e240cddb15a22d4dd
funnythingz <hiroki@hackleberry.me>
2014-04-30 15:11:05 +0900

    test

--- app/views/entries/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @entry, :id, :name, :no, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140429111224) do
+ActiveRecord::Schema.define(version: 20140430060926) do
 
-  create_table "entries", force: true do |t|
-    t.string   "name"
-    t.string   "no"
+  create_table "tests", force: true do |t|
+    t.string   "title"
+    t.string   "content"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
