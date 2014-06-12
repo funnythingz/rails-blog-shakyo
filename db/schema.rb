a6da3232d2cdd2ba817ec2019f2374dc8fd00632
funnythingz <hiroki@hackleberry.me>
2014-06-12 17:54:24 +0900

    tocyu

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140430060926) do
+ActiveRecord::Schema.define(version: 20140612070546) do
+
+  create_table "articles", force: true do |t|
+    t.string   "title"
+    t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "tests", force: true do |t|
     t.string   "title"
