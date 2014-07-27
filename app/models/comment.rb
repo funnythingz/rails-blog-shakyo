e943bdba378ab02e209b9d5bd75880b491e11fbb
funnythingz <funnythingz69+github@gmail.com>
2014-07-27 17:32:18 +0900

    generate comment controller

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,16 @@
 class Comment < ActiveRecord::Base
     belongs_to :article
 end
+
+class CreateComments < ActiveRecord::Migration
+    def change
+        create_table :comments do |t|
+            t.string :commenter
+            t.text :body
+
+            t.references :article, index: true
+
+            t.timestamps
+        end
+    end
+end
